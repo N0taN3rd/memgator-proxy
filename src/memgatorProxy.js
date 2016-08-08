75d3ba555a35aa1f0717469335fd03b89b40d78e
jberlin <n0tan3rd@gmail.com>
2016-08-08 18:14:01 -0400

    Updated code and added arg parsing

--- src/memgatorProxy.js
+++ src/memgatorProxy.js
@@ -10,25 +10,24 @@ import winston from 'winston'
 import Datastore from 'nedb'
 require('pretty-error').start()
 
-/*
- application/json
- application/link
- application/cdxj+ors
- import winston from 'winston'
- import util from 'util'
- import fs from 'fs-extra'
- import md5 from 'md5'
- import httpProxy from 'http-proxy'
- */
+const argv = require('yargs')
+  .usage('Usage: $0 -host [url] -port [port#]')
+  .demand([ 'host', 'port' ])
+  .alias('h', 'host')
+  .alias('p', 'port')
+  .number('port')
+  .string('host')
+  .describe('host', 'the host name to proxy')
+  .describe('port', 'the port we are listening on')
+  .help()
+  .argv
 
 let app = express()
 
-const db = new Datastore({
+let db = new Datastore({
   filename: path.join('dbs', 'url-hash-count.db'),
   autoload: true
 })
-
-db.loadDatabase()
 
 const logger = new (winston.Logger)({
   transports: [
@@ -41,7 +40,8 @@ const logger = new (winston.Logger)({
       name: 'error-file',
       filename: 'logs/errors.log',
       level: 'error'
-    })
+    }),
+    new (winston.transports.Console)()
   ],
   exceptionHandlers: [
     new winston.transports.File({
@@ -56,15 +56,22 @@ logger.exitOnError = false
 
 const pathRE = new RegExp('/timemap/(?:(?:json)|(?:link)|(?:cdxj))/(.+)')
 
-app.all('*', proxy('http://localhost:9000', {
+//memgator port 80,
+//'http://localhost:9000'
+
+let host = argv.host || argv.h
+let port = argv.port || argv.p
+
+app.all('*', proxy(host, {
   intercept(rsp, data, req, res, callback) {
+    console.log('intercept')
     callback(null, data)
     let statusCode = rsp.statusCode
-    let url = S(req.url)
-    console.log(url.s)
-    if (url.contains('timemap') && req.method === 'GET') {
+    console.log(req.url)
+    let urlT = S(req.url)
+    if (urlT.contains('timemap') && req.method === 'GET') {
       let now = moment().format('YYYYMMDDHHmmss')
-      let urlO = pathRE.exec(url.s)
+      let urlO = pathRE.exec(urlT.s)
       let hash = md5(urlO[ 1 ])
       console.log(urlO[ 1 ])
       let memcount = rsp.headers[ 'x-memento-count' ]
@@ -95,126 +102,61 @@ app.all('*', proxy('http://localhost:9000', {
           })
         }
       })
-      let id = { _id: urlO[ 1 ] }
+      let url = urlO[ 1 ]
+      let id = { url }
       db.find(id, (errFind, docs) => {
           if (errFind) {
             logger.error('finding url[%s] failed %s', url, errFind)
           } else {
             if (docs.length === 0) {
+              console.log('docs.length is zero inserting new document')
               let insertMe = {
-                _id: urlO[ 1 ],
+                url,
                 hash,
                 mementoCount: [ { count: memcount, date: now } ],
-                statusCodes: [
-                  {
-                    code: statusCode,
-                    date: now
-                  }
-                ]
-
               }
               db.insert(insertMe, (insertError, newDoc) => {
                 if (insertError) {
                   logger.error('inserting new url[%s] failed %s', url, errFind)
+                } else {
+                  console.log('insert worked ', newDoc)
                 }
               })
             } else {
               let update = {
                 $push: {
-                  mementoCount: { count: memcount, date: now },
-                  statusCodes: {
-                    code: statusCode,
-                    date: now
-                  }
+                  mementoCount: { count: memcount, date: now }
                 }
               }
-              db.update(id, update,{ upsert: true }, (errUpdate, numAffected, affectedDocuments, upsert) => {
-                console.log(errUpdate)
-                console.log(numAffected)
-                console.log(affectedDocuments)
-                console.log(upsert)
+              db.update(id, update, { upsert: false }, (errUpdate, numAffected, affectedDocuments, upsert) => {
                 if (errUpdate) {
                   logger.error('updating mementocount timemap for url[%s] failed %s', url, errFind)
+                } else {
+                  console.log('updating db worked ', numAffected, affectedDocuments)
                 }
               })
             }
           }
         }
       )
-      // db.urlHash.find({ _id: hash }, (errUHF, docs) => {
-      //   if (errUHF) {
-      //     logger.error('finding hash[%s] -> url[%s] failed %s', hash, url, errUHF)
-      //   } else {
-      //     if (docs.length === 0) {
-      //       db.urlHash.insert({ _id: hash, url: urlO[ 1 ] }, (err, newDoc) => {
-      //         if (err) {
-      //           logger.error('inserting hash[%s] -> url[%s] failed %s', hash, url, err)
-      //         } else {
-      //           let toBeInserted = { _id: hash, count: [ { count: memcount, date: m.format('YYYYMMDD') } ] }
-      //           db.mementoCount.insert(toBeInserted, (err, newDoc))
-      //         }
-      //       })
-      //     }
-      //   }
-      // })
-      // db.mementoCount.find({ _id: hash }, (err, docs) => {
-      //   if (docs.length === 0) {
-      //
-      //   } else {
-      //     db.mementoCount.update({ _id: hash },
-      //       { $push: { mementoCount: { count: memcount, date: m.format('YYYYMMDD') } } },
-      //       (err, numAffected, affectedDocuments, upsert) => {
-      //         console.log(err)
-      //         console.log(numAffected)
-      //         console.log(affectedDocuments)
-      //         console.log(upsert)
-      //       })
-      //   }
-      // })
     }
   },
   preserveHostHdr: true
 }))
 
 let proxyS = http.createServer(app)
-proxyS.listen(8008)
+proxyS.listen(port)
 
-//
-//
+process.on('SIGTERM', () => {
+  console.log('Stopping proxy server')
+  proxyS.close(() => {
+    process.exit(0)
+  })
+})
 
-// //
-// // logger.log('info', 'starting')
-//
-// let proxy = httpProxy.createProxyServer({
-//   target: 'http://localhost:9000'
-// })
-//
-// // proxy.on('proxyReq', (proxyReq, req, res) => {
-// //   logger.log('info', 'proxyReq %s %s',req.url,JSON.stringify(req.headers, true, 2))
-// // })
-// //
-// // proxy.on('proxyRes', (proxyRes, req, res) => {
-// //   logger.log('info', 'proxyRes %s %s',proxyRes.url,JSON.stringify(proxyRes.headers, true, 2))
-// // })
-// // proxy.on('proxyReq',(proxyReq,req,res) => {
-// //   // console.log('proxyReq')
-// //   // console.log('showing req')
-// //   // console.log(util.inspect(req,{depth: null,colors: true}))
-// //   // console.log('===========================================')
-// //   // console.log('showing res')
-// //   // console.log(util.inspect(res,{depth: null,colors: true}))
-// //   // console.log('===========================================')
-// // })
-//
-// proxy.on('proxyRes',(proxyRes,req,res) => {
-//   // console.log(req,res,process)
-//   console.log('RAW Response from the target', JSON.stringify(proxyRes.headers))
-//   console.log(util.inspect(proxyRes,{colors: true}))
-//
-// })
-//
-//
-//
-//
-// proxy.listen(8008)
-
+process.on('SIGINT', () => {
+  console.log('Stopping proxy server')
+  proxyS.close(() => {
+    process.exit(0)
+  })
+})
